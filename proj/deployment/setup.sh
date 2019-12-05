8f0a0dab631882be22eee3c60cfb5bfe9f823c29
Pedro Bernardo <pedro_bernardo@icloud.com>
2019-12-05 17:05:03 +0000

    update certificate generation

--- proj/deployment/setup.sh
+++ proj/deployment/setup.sh
@@ -19,21 +19,30 @@ openssl req -new -x509 -key ca.key -out ca.crt
 
 cd ..
 
-echo "creating server key"
+echo "creating server_tls key"
 openssl genrsa -out server_tls.key 2048
 
-echo "creating server signing request"
+echo "creating server_tls signing request"
 openssl req -new -key server_tls.key -out server_tls.csr
 
-echo "creating server ceritficate signed by the CA"
+echo "creating server_tls ceritficate signed by the CA"
 openssl x509 -req -in server_tls.csr -CA cacerts/ca.crt -CAkey cacerts/ca.key -CAcreateserial -extfile ./openssl.cnf -extensions v3_ca -out server_tls.crt -days 300 -sha256
+
+echo "creating server key"
+openssl genrsa -out server.key 2048
+
+echo "creating server signing request"
+openssl req -new -key server.key -out server.csr
+
+echo "creating server ceritficate signed by the CA"
+openssl x509 -req -in server.csr -CA cacerts/ca.crt -CAkey cacerts/ca.key -CAcreateserial -extfile ./openssl.cnf -extensions v3_ca -out server.crt -days 300 -sha256
 
 # openssl x509 -req -in server_tls.csr -CA cacerts/ca.pem -CAkey cacerts/ca.key -CAcreateserial -out server_tls.crt -days 300 -sha256
 
 echo "moving CA certificate to client"
 cp cacerts/ca.crt ../Client/ssl
 echo "moving keys to server"
-cp server_tls.key server_tls.crt ../Server/ssl
+cp server_tls.key server_tls.crt server.key server.crt ../Server/ssl
 
 echo "copying files to docker images"
 cp -r ../Client/client ../Client/ssl docker/client/client/
